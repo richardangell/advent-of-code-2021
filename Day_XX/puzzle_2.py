23540f9be54658aa35be78d31d4a9757d339c9bd
richardangell <richardangell37@gmail.com>
2021-12-05 20:35:25 +0000

    add template folder

---
+++ Day_XX/puzzle_2.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.path.append("..")
+import helpers  # noqa
+
+
+
+
+if __name__ == "__main__":
+
+    input = helpers.load_input("input_1.txt", remove_lines_breaks=True)
+
+    print(input)
---
+++ Day_XX/tests/test_day_xx.py
@@ -0,0 +1,16 @@
+import puzzle_1
+import puzzle_2
+
+
+def test_puzzle_1():
+
+    input_1 = puzzle_1.load_input("tests/input_1.txt")
+
+    assert
+
+
+def test_puzzle_2():
+
+    input_1 = puzzle_2.load_input("tests/input_1.txt")
+
+    assert
